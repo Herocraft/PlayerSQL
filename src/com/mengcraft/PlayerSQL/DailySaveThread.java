3c9036532f9e31112ca30ed89015cd5fdec4597a
caoli5288 <caoli5288@gmail.com>
2014-05-04 20:52:25 +0800

    增加输出控制开关

--- src/com/mengcraft/PlayerSQL/DailySaveThread.java
+++ src/com/mengcraft/PlayerSQL/DailySaveThread.java
@@ -10,6 +10,7 @@ public class DailySaveThread extends Thread
 	@Override
 	public void run()
 	{
+		boolean show = PlayerSQL.plugin.getConfig().getBoolean("daily.show");
 		int delay = PlayerSQL.plugin.getConfig().getInt("daily.delay");
 		int min = PlayerSQL.plugin.getConfig().getInt("daily.min");
 		Player[] players;
@@ -32,7 +33,7 @@ public class DailySaveThread extends Thread
 					if (!players[i].isOnline()) {
 						continue;
 					}
-					if (DoPlayer.savePlayer(players[i])) {
+					if (DoPlayer.savePlayer(players[i]) && show) {
 						Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "保存玩家 " + players[i].getName() + " 成功");
 						Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "进度 " + (i + 1) + " / " + players.length);
 					}
